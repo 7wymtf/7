5f48327ecf266d087ddf7ad5894f935dfc39a5d3
7wymtf <xodvj7@gmail.com>
2025-04-25 22:13:03 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-14 items-center">
           <div className="flex-shrink-0">
-            <span className="text-white text-lg font-bold">HackerTools</span>
+            <span className="text-white text-lg font-bold">7wym</span>
           </div>
           
           <div className="hidden md:block">
